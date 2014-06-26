b06ad76888afc8e8dbe427858e8be2062781a2ce
Arnav Kumar Agrawal <arnavkagrawal@gmail.com>
2014-06-26 16:47:04 +0530

    Better test images

--- web2py/applications/AssignmentPortal/temps/detectlines.py
+++ web2py/applications/AssignmentPortal/temps/detectlines.py
@@ -1,14 +1,24 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('dave.jpg')
+image_name=raw_input("Enter image name: ")
+img = cv2.imread(image_name)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
+lines = cv2.HoughLines(edges,1,np.pi/180,35)
 print type(lines[0])
 
 for rho,theta in lines[0]:
+    if(((theta*180)/np.pi)<10):
+        if(((theta*180)/np.pi)>1):
+            continue
+    else:
+        if(((theta*180)/np.pi)>89) and (((theta*180)/np.pi)<91):
+            pass
+        else:
+            continue
+    print rho,((theta*180)/np.pi)
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a*rho
@@ -20,4 +30,5 @@ for rho,theta in lines[0]:
 
     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
-cv2.imwrite('houghlines3.jpg',img)
+cv2.imwrite('output.jpg',img)
+print "output image saved in output.jpg"
