d084d06cb78cbb938c180942eb85534c136a12f5
NightFury13 <develop13mohit@gmail.com>
2014-08-12 14:08:35 +0530

    fixed views

--- web2py/applications/AssignmentPortal/models/menu.py
+++ web2py/applications/AssignmentPortal/models/menu.py
@@ -5,8 +5,8 @@
 ## Customize your APP title, subtitle and menus here
 #########################################################################
 
-response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
-                  _class="brand",_href="http://www.web2py.com/")
+response.logo = A(B('Shiksha'),XML('&trade;&nbsp;'),
+                  _class="brand",_href=URL('default','index'))
 response.title = request.application.replace('_',' ').title()
 response.subtitle = ''
 
